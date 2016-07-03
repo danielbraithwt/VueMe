44275b824e4a51c973a43cca4ca683c6743f2e70
Daniel Braithwaite <danielbraithwt@localhost.localdomain>
2016-07-04 11:43:38 +1200

    Create Basic Page Header Create basic layout for the page header

---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="Container">
+    <div class="row middle-xs">
+      <div class="col-xs-12 col-sm-6 center-xs">
+        <div class="box">
+          <img src="https://placehold.it/150x150" />
+          <h1 class="h1--responsive">Daniel Braithwaite</h1>
+          <social></social>
+        </div>
+      </div>
+
+      <div class="col-xs-12 col-sm-6 center-xs">
+        <div class="box">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Social from './Social.vue';
+
+export default {
+  name: 'header-section',
+  components: {
+    Social
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Social.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="Social">
+    <div class="Social-icon">
+
+    </div>
+
+    <div class="Social-icon">
+
+    </div>
+
+    <div class="Social-icon">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'social'
+}
+</script>
+
+<style>
+.Social {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.Social-icon {
+  width: 50px;
+  height: 50px;
+
+  border-radius: 50%;
+
+  background-color: lightgrey;
+
+  margin: 10px;
+}
+</style>
