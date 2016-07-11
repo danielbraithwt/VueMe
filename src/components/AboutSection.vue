ce8bb1edc259ff8a6ecfc3d487d27447f499442c
Daniel Braithwaite <danielbraithwt@localhost.localdomain>
2016-07-11 21:10:28 +1200

    Create Mobile Layout For About Section Created the initial layout (mobile first) layout for about section

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,19 +1,38 @@
 <template>
-<div class="Container">
+<div class="Container Fill--height">
   <div class="row">
     <div class="col-xs-12">
-      <h1>Experence</h1>
+      <h1 class="Section-header">About Me</h1>
     </div>
-    <div class="col-xs-12 col-sm-6">
+    <div class="col-xs-12">
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
-    <div class="col-xs-12 col-sm-6">
+
+    <div class="col-xs-12">
+      <h1 class="Section-header">Experence</h1>
+    </div>
+    <div class="col-xs-12">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="col-xs-12">
       <div class="Skills row">
-        <skill></skill>
-        <skill></skill>
-        <skill></skill>
+        <skill name="JS" color="yellow"></skill>
+        <skill name="Ember" color="amber"></skill>
+        <skill name="Java" color="red"></skill>
+        <skill name="Git" color="brown"></skill>
+        <skill name="SASS" color="pink"></skill>
+        <skill name="Vue" color="green"></skill>
       </div>
     </div>
+
+    <div class="col-xs-12">
+      <h1 class="Section-header">Quallifications</h1>
+    </div>
+    <div class="col-xs-12">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+
+
   </div>
 </div>
 </template>
@@ -33,5 +52,9 @@ export default {
 .Skills {
   display: flex;
   flex-wrap: wrap;
+}
+
+.Section-header {
+  margin-bottom: 0;
 }
 </style>
--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -1,24 +1,56 @@
 <template>
-<div class="Skill">
-
+<div class="Icon Skill Skill--{{color}}">
+  <p class="Skill-text">{{name}}</p>
 </div>
 </template>
 
 <script>
 export default {
-  name: 'skill'
+  name: 'skill',
+  props: ['name',
+          'color'
+        ]
 }
 </script>
 
 <style>
 .Skill {
-  width: 50px;
-  height: 50px;
-
-  border-radius: 50%;
-
-  margin: 10px;
-
   background-color: lightgrey;
+  text-align: center;
+}
+
+.Skill-text {
+  font-weight: bold;
+  color: white;
+}
+
+/* JS */
+.Skill--yellow {
+  background-color: #F1BF26;
+}
+
+/* Java */
+.Skill--red {
+  background-color: #ED1C24;
+}
+
+/* SASS */
+.Skill--pink {
+  background-color: #C6538C;
+}
+
+/* Ember */
+.Skill--amber {
+  background-color: #DD6A58;
+}
+
+/* Git */
+.Skill--brown {
+  background-color: brown;
+}
+
+/* Vue */
+.Skill--green {
+  background-color: #4FC08D;
 }
 </style>
