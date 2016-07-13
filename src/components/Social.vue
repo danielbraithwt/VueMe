95d959c14283b35b55ee1b56ad9bdd7d5cbc097f
Daniel Braithwaite <danielbraithwt@localhost.localdomain>
2016-07-13 21:12:45 +1200

    Update Social Icon Styling Social icons now have drop shadow and white background

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -21,14 +21,16 @@ export default {
   width: 100%;
   display: flex;
   flex-wrap: wrap;
-  padding: 20px;
+  padding-bottom: 10px;
 }
 
 .Social-icon {
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: lightgrey;
+
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  background-color: white;
 }
 
 .Social-icon-image {
