07876059a95efd1271cb7defc360fb8393e5ddd7
Daniel Braithwaite <danielbraithwt@gmail.com>
2017-03-31 22:27:25 +1300

    Update Copy and Images
    Change about me images and update the website copy

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <header-section :name="name" :photo="photo" :social="social"></header-section>
     <about-section :about="about"></about-section>
+    <research-section :research="research"></research-section>
     <project-section :projects="projects"></project-section>
   </div>
 </template>
@@ -10,13 +11,15 @@
 import HeaderSection from './components/HeaderSection.vue';
 import AboutSection from './components/AboutSection.vue';
 import ProjectSection from './components/ProjectSection.vue';
+import ResearchSection from './components/ResearchSection.vue';
 
 export default {
   name: 'app',
   components: {
     HeaderSection,
     AboutSection,
-    ProjectSection
+    ProjectSection,
+    ResearchSection,
   },
   data: function() {
     return {
@@ -33,7 +36,7 @@ export default {
           name: 'Email',
           label: 'Email Link Clicked',
           icon: '../../static/mail.svg',
-          url: '#',
+          url: 'mailto:danielbraithwt@gmail.com',
         },
         {
           name: 'Resume',
@@ -44,63 +47,57 @@ export default {
       ],
       about: {
         aboutme: {
-          image: '../static/About-profile_02.png',
+          image: '../static/About-Me.png',
           subtitle: 'What makes me Daniel',
-          text: "I'm a caffeine-addicted, fitness-loving nerd who thrives on writing code and solving difficult problems. My passion started with design, but after learning C with my dad I fell in love with the art of programming. I love being able to affect and enhance people's lives by creating beautiful and useful software.",
+          text: "I'm a caffeine-addicted, fitness-loving nerd who thrives on solving interesting and difficult problems. My passion started with design, but after learning C with my dad I fell in love with the art of programming. My interestes have sinse shifted from simply enjoying programming to seeking a more mathematical understanding of how things work.",
         },
         experence: {
-          image: '../static/About-work_02.png',
+          image: '../static/About-Work.png',
           subtitle: 'What I have done',
           text: "During my second year of study I was lucky enough to get an internship at the amazing Wellington company Alphero as a web developer. Alphero has some incredible designers and takes pride in creating beautiful web and mobile apps. As a developer I loved being a part of this environment. This opportunity allowed me work on a lot of projects using a wide range of cutting edge technologies.",
-          skills: [
-            {
-              name: 'JS',
-              color: 'yellow'
-            },
-            {
-              name: 'Ember',
-              color: 'amber'
-            }
-          ]
+          //skills: [
+          //  {
+          //    name: 'JS',
+          //    color: 'yellow'
+          //  },
+          //  {
+          //    name: 'Ember',
+          //    color: 'amber'
+          //  }
+          //]
         },
         quallifications: {
-          image: '../static/About-quall_02.png',
+          image: '../static/About-Qull.png',
           subtitle: 'What got me to where I am',
-          text: "My passion started at high school, where I had three amazing teachers and two supportive parents giving me the opportunities to achieve highly in the fields that I love. I'm now at Victoria University of Wellington where I'm in my third and final year of my BSc in Computer Science and Mathematics."
+          text: "My passion started at high school, where I had three amazing teachers and two supportive parents giving me the opportunities to achieve highly in the fields that I love. I'm now at Victoria University of Wellington where I have finished my BSc in Computer Science and Mathematics and am pursuing a BSc with Honors in the same fields. Following this I am interested in doing further postgraduate study."
         },
         interests: {
-          image: 'http://placehold.it/500x250',
+          image: '../static/About-interests.png',
           subtitle: 'What are my interests',
-          text: "After taking an Introduction to Artificial Intelligence and a Algorithm Design paper at my university I found that these two topics really interested me. I particularly enjoyed being able to use AI techniques with a large collection of data to produce meaningful information from it. The algorithm design course involved taking problems and using different techniques to solve them and then prove our algorithms were correct. Proving algorithms correct particularly appealed to me as most of the course I took were not this rigorous or mathematical."
+          text: "I am very interested in Machine Learning and as part of my honors degree I am able to persue research into areas of machine learning which I find interesting. One such area is Logical Neural Netowrks, this is a Neural Network where the activations are 'Noisy' logic gates. The idea of using these activation functions is to yield trained networks that are more interpretable so that someone might analyze what features the network has learnt. Outside of study I love to be moving and exercising, I am training to compete in Olympic Weightlifting."
         }
       },
       projects: [
         {
-          image: 'http://placehold.it/500x250',
-          name: 'Cooperative Bank',
+          // image: 'http://placehold.it/500x250',
+          name: 'NZ Banking Website',
           subtitle: 'Alphero',
-          url: 'https://www.co-operativebank.co.nz/',
-          // git: '',
-          description: "This was an Ember JS project. I was responsible for making upgrades and bug fixes along with helping build the overdraft flow",
+          description: "This was an Ember JS project for a New Zealand bank. I was responsible for making upgrades and bug fixes along with helping build the overdraft flow. This project allowed me to develop a good understanding of EmberJS but also working in a team.",
         },
         {
-          image: 'http://placehold.it/500x250',
-          name: 'Paymark',
+          // image: 'http://placehold.it/500x250',
+          name: 'Large NZ Eftpos Retailer',
           subtitle: 'Alphero',
-          url: 'https://www.paymark.co.nz/',
-          // git: '',
-          description: "For this website we used Webhook CMS to load our content and then plain JS and SASS. This was one of my favorite projects i have worked on as this was the first industry project I was able to take from start to finish (mostly the summer was over before the site went live).",
+          description: "For this website we used Webhook CMS to load our content and then plain JS and SASS. This was one of my favorite projects I have worked on as this was the first industry project I was able to take from start to finish (mostly the summer was over before the site went live).",
         },
         {
-          image: 'http://placehold.it/500x250',
-          name: 'Lighthouse Chromecast Reciver',
+          // image: 'http://placehold.it/500x250',
+          name: 'NZ Streaming Service Chromecast Reciver',
           subtitle: 'Alphero',
-          // url: '',
-          // git: '',
-          description: "I was responsible for creating the Chromecast receiver application for Lighthouse (a tv/movie streaming service). ",
+          description: "I was responsible for working on the Chromecast Receiver application for an NZ TV/Movie streaming service. Working on the communication between android app and receiver as well as the interface.",
         },
         {
-          image: 'http://placehold.it/500x250',
+          // image: 'http://placehold.it/500x250',
           name: 'VueMe',
           subtitle: 'Personal',
           url: 'danielbraithwaite.name',
@@ -108,12 +105,24 @@ export default {
           description: "After learning so much at Alphero I wanted a way to apply what I had learnt, remaking my personal website seemed like a natural way to apply these skills. The current JS frameworks I knew didn't seem like a good fit so this was an opportunity to learn a new front end framework. I chose Vue because of its simplicity, which was good because there wasn't a huge learning curve for the framework, also Vue is much less opinionated than other frameworks allowing me to structure the project how I wanted.",
         },
         {
-          image: 'http://placehold.it/500x250',
+          // image: 'http://placehold.it/500x250',
           name: 'Whats Next',
           subtitle: 'GovHack 2016 - Personal',
           // url: '',
           git: 'https://github.com/working-title/whats-next',
           description: "What's next was created at GovHack 2015 and was designed to help high school leavers make informed decisions about what to do next. Often there is a lot of pressure on school leavers from family, friends or teachers  to go to university or learn a trade, this can make deciding what is best for you even more difficult than it already is. What's next allows school leavers to compare a number of creators based on a number of different factors so they can make an informed choice about what's next.",
+        }
+      ],
+      research: [
+        {
+          name: 'Logical Neural Networks: Opening the black box',
+          description: 'Logical neural networks are networks built with neurons using activation functions based off logical gates. I am continuing on from research done by a previous honos student who gave a proof of concept for Logical Neural Netowrks, they where shown to provide reasonable accuraccy and to be alot more understandble as oposed to networks using regular activation functions. I am continuing this research by first showing we can use Logical Nets to learn Boolean expressions. Currently there only OR and AND neurons which when combined arnt functionally complete so I will implement other gates such as NAND.',
+          git: 'https://github.com/danielbraithwt/Logical-Neural-Networks',
+        },
+        {
+          name: 'Machine Learning of Collectives of local decision makers',
+          description: 'The idea behind this research project is to create nerons which rather than being tightly connected they are as seperate as possible to allow adding and removal of neurons. Training will involve nurons identifying some part of the data which is "interesting" and learn to classify that area of the data.',
+          git: 'https://github.com/garibaldu/boundary-seekers',
         }
       ]
     }
--- src/components/ResearchSection.vue
+++ src/components/ResearchSection.vue
@@ -1,14 +1,14 @@
 <template>
 
-<div>
+<div class="u-background">
   <div class="Container">
     <div class="row">
       <div class="col-xs-12 center-xs">
-        <h1 class="Title">Projects!</h1>
+        <h1 class="Title u-textWhite">Research!</h1>
       </div>
     </div>
     <div class="MasonryColumn MasonryColumn--balanced">
-      <template v-for="project in projects">
+      <template v-for="project in research">
         <project class="col-xs-12 MasonryColumn-Item" :project="project"></project>
       </template>
     </div>
@@ -20,8 +20,8 @@
 import Project from './Project.vue';
 
 export default {
-  name: 'project-section',
-  props: ['projects'],
+  name: 'research-section',
+  props: ['research'],
   components: {
     Project
   }
